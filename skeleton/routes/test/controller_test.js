33d2e20ba77d35b4934c27b3df565646d1920f61
Atsushi Nakatsugawa <atsushi@moongift.jp>
2016-12-31 16:15:58 +0900

    Update skeleton

--- skeleton/routes/test/controller_test.js
+++ skeleton/routes/test/controller_test.js
@@ -1,6 +1,6 @@
 var path = require('path');
 require('set-node-path')(
-  path.resolve(`${airlane_path}/node_modules`)
+  path.resolve(airlane_path + "/node_modules");
 );
 var chai    = require('chai');
 var jsdom   = require('jsdom');
